293498c69ae71b5a568e27c60f9c3718ce959165
Алекс Инджов <sashe94@hotmail.com>
2017-04-23 00:59:31 +0300

    Initial IT Geek Commit

---
+++ ITGeek/ITGeek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITGeek.MvcApplication" Language="C#" %>
---
+++ ITGeek/ITGeek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITGeek.Startup))]
+namespace ITGeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
